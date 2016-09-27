602e516541a14c23a3d7ad024fa8ca3340781f3c
nijeyaku <jeyakumar.niv@gmail.com>
2016-09-27 14:36:27 +0200

    Add Main class (td_27_09_16)

---
+++ src/tdd0/Main.java
@@ -0,0 +1,10 @@
+package src.tdd0;
+
+/**
+ * Created by nijeyaku on 27/09/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("coucou");
+    }
+}
